937b30423a32532f30c0557de76c7de26cd98028
ShubhAhuja3112 <shubh.ahuja9999999@gmail.com>
2025-08-27 16:49:57 +0530

    Add files via upload

---
+++ Experiment 1/Experiment 1.1.cpp
@@ -0,0 +1,10 @@
+/*experiment 1.1*/
+/*wap to print hello world*/
+#include <stdio.h>
+
+int main() 
+{
+    printf("Hello World\n");
+    return 0;
+}
+
---
+++ Experiment 1/Experiment 1.4.cpp
@@ -0,0 +1,14 @@
+/*experiment 1.4*/
+/*write a program in C to perform 4 arthematic operation on 2 varibale +,-,*,/ */
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("enter the value of a and b");
+	scanf("%f%f",&a,&b);
+	printf("the value of addition of a and b is %f\n",a+b);
+	printf("the value of multiplication of a and b is %f\n",a*b);
+	printf("the value of division of a and b is %f\n",a/b);
+	printf("the value of substraction of a and b is %f\n",a-b);
+	return 0;
+}
